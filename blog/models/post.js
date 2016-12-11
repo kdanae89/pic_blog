a843cac32a5a9201c07b9dc6230c2a1bc8002e13
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2016-12-11 10:18:56 -0800

    made my models

--- blog/models/post.js
+++ blog/models/post.js
@@ -1,7 +1,15 @@
 //require dependencies
+var mongoose = require('mongoose');
 
 //create schema
+var postSchema = mongoose.Schema({
+  title: String,
+  img: {type: String, required: true},
+  date: Date
+});
 
 //create model
+var Post = mongoose.model('Post', postSchema);
 
 //export model
+module.exports = Post;
--- blog/models/user.js
+++ blog/models/user.js
@@ -1,7 +1,14 @@
 //require dependencies
+var mongoose = require('mongoose');
 
 //create schema
+var userSchema = mongoose.Schema({
+  name: {type: String, required: true},
+  password: {type: String, required: true}
+});
 
 //create model
+var User = mongoose.model('User', userSchema);
 
 //export model
+module.exports = User;
