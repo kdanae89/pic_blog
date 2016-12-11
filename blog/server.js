d6eb6563e3dac104e98cc659c253c271fff28916
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2016-12-11 07:19:20 -0800

    port and mongo up

--- blog/server.js
+++ blog/server.js
@@ -1,7 +1,14 @@
 //require dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var methodOverride = require('method-override');
 
 
 //middlewares - body-parser, method-override, session
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({extended:false}));
 
 
 //model controllers
@@ -9,6 +16,9 @@
 
 //render main page here, or i could do it in posts controller since they share an index page
 //welcome current user if logged in
+app.get('/', function(req, res){
+  res.render('index.ejs');
+});
 
 
 //content for user or non user
@@ -18,7 +28,15 @@
 
 
 //test for connection
+mongoose.connect('mongodb://localhost:27017/blog');
+
+mongoose.connection.once('open', function(){
+  console.log('mongooooo');
+});
 
 
 
 //listen to port
+app.listen(3000, function(){
+  console.log('suh dude');
+});
