7c5fdee3f54e631634479032ab3f7069db4a641b
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2016-12-11 10:37:16 -0800

    set up sessions

--- blog/controllers/sessions.js
+++ blog/controllers/sessions.js
@@ -1,11 +1,33 @@
 //require dependencies
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user.js');
 
 //new session require
+router.get('/new', function(req, res){
+  res.render('sessions/new.ejs');
+});
 
 
 //allow or deny access
+router.post('/', function(req, res){
+  User.findOne({ username: req.body.username }, function(err, foundUser){
+    if(req.body.password == foundUser.password){
+      req.session.currentUser = foundUser;
+      res.redirect('/');
+    } else {
+      res.send('access denied');
+    }
+  });
+});
 
 
-//delete session
+//terminate session
+router.delete('/', function(req, res){
+  req.session.destroy(function(){
+    res.redirect('/');
+  });
+});
 
 //export router
+module.exports = router;
--- blog/models/user.js
+++ blog/models/user.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 //create schema
 var userSchema = mongoose.Schema({
-  name: {type: String, required: true},
+  username: {type: String, required: true},
   password: {type: String, required: true}
 });
 
