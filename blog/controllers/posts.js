71ed91a865efe5c06a66be562ce1db141147a3e3
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2016-12-12 08:32:45 -0800

    show route for posts up

--- blog/controllers/posts.js
+++ blog/controllers/posts.js
@@ -5,8 +5,13 @@ var Post = require('../models/posts.js');
 
 
 //index route
-
-//show route
+router.get('/', function(req, res){
+  Post.find({}, function(err, foundPosts){
+    res.render('posts/index.ejs',{
+      post: foundPosts
+    });
+  });
+});
 
 //new route
 router.get('/new', function(req, res){
@@ -15,10 +20,22 @@ router.get('/new', function(req, res){
 
 //create
 router.post('/', function(req, res){
-  Post.create(req.body, function(err, createdUser){
+  Post.create(req.body, function(err, createdPost){
     res.redirect('/');
   });
 });
+
+//show
+router.get('/:id', function(req, res){
+  Post.findById(req.params.id, function(err, foundPost){
+    console.log(foundPost);
+    if (err) {console.log(err)};
+    res.render('posts/show.ejs',{
+      post: foundPost
+    });
+  });
+});
+
 
 //edit route(might have to switch position of this)
 
